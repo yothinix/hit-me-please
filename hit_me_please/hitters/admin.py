6ba91b49e962b419937d888bb99b67c53af27f73
Yothin M <m@yothinix.com>
2019-06-01 11:34:04 +0700

    ✨ Add admin for Hitter model :rocket:

--- hit_me_please/hitters/admin.py
+++ hit_me_please/hitters/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hitter
+
+
+@admin.register(Hitter)
+class HitterAdmin(admin.ModelAdmin):
+    pass
--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.test import TestCase
 
+from .admin import HitterAdmin
 from .models import Hitter
 
 
@@ -9,3 +11,12 @@ class HitterTest(TestCase):
             email='m@yothinix.com'
         )
         self.assertEqual(hitter.email, 'm@yothinix.com')
+
+
+class HitterAdminTest(TestCase):
+    def test_hitter_should_be_registered_to_admin(self):
+        self.assertIsInstance(
+            admin.site._registry[Hitter],
+            HitterAdmin
+        )
+
