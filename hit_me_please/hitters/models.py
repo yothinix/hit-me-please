a42638d344b1615fdadc9aac4d22de4a321349c9
Yothin M <m@yothinix.com>
2019-06-01 11:20:52 +0700

    :card_file_box: Introduce Hitter model :rocket:

--- hit_me_please/hitters/models.py
+++ hit_me_please/hitters/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Hitter(models.Model):
+    email = models.EmailField(max_length=300)
--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Hitter
+
+
+class HitterTest(TestCase):
+    def test_hitter_model_should_have_email_field(self):
+        hitter = Hitter.objects.create(
+            email='m@yothinix.com'
+        )
+        self.assertEqual(hitter.email, 'm@yothinix.com')
