2cc2c19d54821d524ddb807d7f86fce13c678108
Yothin M <m@yothinix.com>
2019-06-01 15:07:04 +0700

    :lipstick: Add Hit me header :rocket:

--- hit_me_please/hitters/tests/test_views.py
+++ hit_me_please/hitters/tests/test_views.py
@@ -7,6 +7,8 @@ class LandingPageViewTest(TestCase):
     def test_view_should_have_form_with_email_field_and_submit_button(self):
         response = self.client.get('/')
 
+        expected = '<h1>Hit Me!</h1>'
+        self.assertContains(response, expected, status_code=200)
         expected = '<form action="." method="post">'
         self.assertContains(response, expected, status_code=200)
         expected = '<input type="hidden" name="csrfmiddlewaretoken"'
