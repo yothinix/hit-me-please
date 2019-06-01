1866ea246828f1d07f44683aaf3e03e29b267d74
Yothin M <m@yothinix.com>
2019-06-01 15:13:09 +0700

    :white_check_mark: Add acceptance for landing page :rocket:

---
+++ tests/cypress/integration/submit_form.spec.js
@@ -0,0 +1,33 @@
+describe('Landing Page', () => {
+  it('should be able to submit form with email', () => {
+    const user = cy
+    user.visit('http://localhost:8000/')
+    user.get('h1')
+        .contains('Hit Me!')
+    user.get('input[name="email"]')
+        .type('m@yothinix.com')
+    user.get('button[type="submit"]')
+        .click()
+
+    const admin = cy
+    admin.visit('http://localhost:8000/admin/')
+    admin.get('input[name="username"]')
+         .type('admin')
+         .get('input[name="password"]')
+         .type('password')
+         .get('input[type="submit"]')
+         .click()
+    admin.get('.model-hitter > th > a')
+         .click()
+    admin.get('.field-email > a')
+         .should('contain', 'm@yothinix.com')
+    admin.get('#action-toggle')
+         .click()
+    admin.get('select')
+         .select('delete_selected')
+    admin.get('.button')
+         .click()
+    admin.get('[type="submit"]')
+         .click()
+  })
+})
