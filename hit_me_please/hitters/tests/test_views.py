f279b29986ef25cb71450d81af11a7fc94fcc83c
Yothin M <m@yothinix.com>
2019-06-02 13:02:43 +0700

    :lipstick: Change header of form :rocket:

--- hit_me_please/hitters/tests/test_views.py
+++ hit_me_please/hitters/tests/test_views.py
@@ -7,7 +7,7 @@ class LandingPageViewTest(TestCase):
     def test_view_should_have_form_with_email_field_and_submit_button(self):
         response = self.client.get('/')
 
-        expected = '<h1>Hit Me!</h1>'
+        expected = '<h1>Hit Me, Baby!</h1>'
         self.assertContains(response, expected, status_code=200)
         expected = '<form action="." method="post">'
         self.assertContains(response, expected, status_code=200)
