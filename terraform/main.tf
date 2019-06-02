c19fbe6bf65775aa33ec51eb3f6a1be78d21d889
Yothin M <m@yothinix.com>
2019-06-02 10:23:02 +0700

    :wrench: Create GCP compute instance via Terraform :rocket:

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,31 @@ provider "google" {
   region      = "asia-southeast1"
   zone        = "asia-southeast1-b"
 }
+
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance-${count.index}"
+  machine_type = "n1-standard-1"
+  count        = 1
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20190530"
+      size  = 30
+    }
+  }
+
+  metadata = {
+    sshKeys = "hitme:${file("~/.ssh/hitme.pub")}"
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  tags = ["my-web"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.tf_instance.*.network_interface.0.access_config.0.nat_ip}"
+}
